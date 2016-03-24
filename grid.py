282c7f5bfd86fb472e7bc546c745a6346e5c5737
stringertheory <mike.stringer@datascopeanalytics.com>
2016-03-24 11:48:02 -0500

    updated grid to remove duplicates

--- grid.py
+++ grid.py
@@ -4,11 +4,22 @@ a nice grid.
 
 """
 import collections
+import json
+import math
 import os
+import random
 import sys
 
 from slugify import slugify
+
+import imagehash
+import ssim
 from PIL import Image
+
+X = 7
+MAX_Y = 9
+SIZE = 150
+PAD = 5
 
 
 def make_filename(key, extension):
@@ -16,21 +27,55 @@ def make_filename(key, extension):
     key = unicode(key.strip())
     return '{}.{}'.format(slugify(key), extension)
 
-# parameters
-X = 7
-SIZE = 150
-PAD = 5
 
-directory = sys.argv[1]
+def read_duplicates(filename):
+    to_remove = set()
+    with open(filename) as infile:
+        duplicate_groups = json.load(infile)
+    total = 0
+    for group in duplicate_groups:
+        total += len(group)
+        for path in group[1:]:
+            to_remove.add(path)
+    return to_remove
 
-# group full file paths by board key
-grouped_by_board = collections.defaultdict(list)
-for filename in os.listdir(directory):
-    path = os.path.join(directory, filename)
-    key = filename.split('[')[1].split(']')[0]
-    grouped_by_board[key].append(path)
 
-for key, filename_list in grouped_by_board.iteritems():
+def group_by_board(directory, dupes=set()):
+    duplicates_removed = 0
+    grouped_by_board = collections.defaultdict(list)
+    grouped_counter = collections.Counter()
+    for filename in os.listdir(directory):
+        path = os.path.join(directory, filename)
+        if path not in dupes:
+            key = filename.split('[')[1].split(']')[0]
+            if grouped_counter[key]:
+                key_with_counter = '%s-%i' % (key, grouped_counter[key])
+            else:
+                key_with_counter = '%s' % key
+            grouped_by_board[key_with_counter].append(path)
+            if len(grouped_by_board[key_with_counter]) >= X * MAX_Y:
+                grouped_counter[key] += 1
+        else:
+            duplicates_removed += 1
+    print >> sys.stderr, 'Removed %i duplicates' % duplicates_removed
+    return sorted(grouped_by_board.items())
+
+# `notes` directory inside of zipfile export from post-it plus
+notes_directory = sys.argv[1]
+
+# if you've used the GUI to find duplicates, this is the filename of
+# the JSON output
+try:
+    duplicates_filename = sys.argv[2]
+except IndexError:
+    dupes = set()
+else:
+    dupes = read_duplicates(duplicates_filename)
+
+for key, filename_list in group_by_board(notes_directory, dupes):
+
+    # TODO: how should we order these?
+    random.shuffle(filename_list)
 
     # make new filename from key
     new_filename = make_filename(key, 'png')
